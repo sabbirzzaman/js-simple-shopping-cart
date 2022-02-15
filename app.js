2669f906804403b3c673cadd0cb8cdb38d730268
sabbirzzaman <sztonmoy7@gamil.com>
2022-02-16 00:41:46 +0600

    cart product added

--- app.js
+++ app.js
@@ -4,87 +4,96 @@ const allProduct = document.getElementById('all-product');
 const cartSlider = document.getElementById('cart-slider');
 const cartProducts = document.getElementById('cart-products');
 
-cartOpenBtn.addEventListener('click', function() {
-    cartSlider.classList.add('slider-open');
-    document.body.classList.add('no-scroll');
-})
+cartOpenBtn.addEventListener('click', function () {
+  cartSlider.classList.add('slider-open');
+  document.body.classList.add('no-scroll');
+});
 
-cartCloseBtn.addEventListener('click', function() {
-    cartSlider.classList.remove('slider-open');
-    document.body.classList.remove('no-scroll');
-})
+cartCloseBtn.addEventListener('click', function () {
+  cartSlider.classList.remove('slider-open');
+  document.body.classList.remove('no-scroll');
+});
 
 allProduct.addEventListener('click', function (event) {
-    if(event.target.matches('button')) {
-        // Create Elements
-        const cartItem = document.createElement('div');
-        const cartProduct = document.createElement('div');
+  // Get product text, pricing, image
+  const productTitle = event.target.parentElement.parentElement.firstElementChild;
+  const productPrice = event.target.parentElement.firstElementChild;
 
-        const cartArrow = document.createElement('div');
-        const cartUpArrow = document.createElement('i')
-        const cartDownArrow = document.createElement('i')
+  const productImageTag = event.target.parentElement.parentElement.parentElement.firstElementChild.firstElementChild;
+  const productImageSrc = productImageTag.getAttribute("src");
+  console.log(productImageSrc)
 
-        const cartImage = document.createElement('img');
 
-        const cartDetails = document.createElement('div');
-        const cartHeading = document.createElement('h4');
-        const cartPrice = document.createElement('p');
+  if (event.target.matches('button')) {
+    // Create Elements
+    const cartItem = document.createElement('div');
+    const cartProduct = document.createElement('div');
 
-        const cartQuantity = document.createElement('div');
-        const quantity = document.createElement('span');
-        const cartRemoveBtn = document.createElement('button');
+    const cartArrow = document.createElement('div');
+    const cartUpArrow = document.createElement('i');
+    const cartDownArrow = document.createElement('i');
 
-        // Add class to the elements
-        cartItem.classList.add('cart-item');
-        cartProduct.classList.add('cart-product');
+    const cartImage = document.createElement('img');
 
-        cartArrow.classList.add('cart-arrow');
-        cartUpArrow.classList.add('fa-solid', 'fa-angle-up');
-        cartDownArrow.classList.add('fa-solid', 'fa-angle-down');
+    const cartDetails = document.createElement('div');
+    const cartHeading = document.createElement('h4');
+    const cartPrice = document.createElement('p');
 
-        cartImage.setAttribute('src', './images/action.png');
+    const cartQuantity = document.createElement('div');
+    const quantity = document.createElement('span');
+    const cartRemoveBtn = document.createElement('button');
 
-        cartDetails.classList.add('cart-details');
+    // Add class to the elements
+    cartItem.classList.add('cart-item');
+    cartProduct.classList.add('cart-product');
 
-        cartQuantity.classList.add('cart-quantity');
-        quantity.setAttribute('id', 'quantity');
-        cartRemoveBtn.classList.add('remove-product');
+    cartArrow.classList.add('cart-arrow');
+    cartUpArrow.classList.add('fa-solid', 'fa-angle-up');
+    cartDownArrow.classList.add('fa-solid', 'fa-angle-down');
 
-        // Add Inner text
-        cartHeading.innerText = 'Action Camera';
-        cartPrice.innerText = '$599';
-        quantity.innerText = '1';
-        cartRemoveBtn.innerText = 'Remove';
+    cartImage.setAttribute('src', productImageSrc);
 
-        // Append child
-        cartItem.appendChild(cartProduct);
-        cartItem.appendChild(cartQuantity);
+    cartDetails.classList.add('cart-details');
 
-        cartProduct.appendChild(cartArrow)
-        cartArrow.appendChild(cartUpArrow)
-        cartArrow.appendChild(cartDownArrow)
+    cartQuantity.classList.add('cart-quantity');
+    quantity.setAttribute('id', 'quantity');
+    cartRemoveBtn.classList.add('remove-product');
 
-        cartProduct.appendChild(cartImage)
+    // Add Inner text
+    cartHeading.innerText = productTitle.innerText;
+    cartPrice.innerText = productPrice.innerText;
+    quantity.innerText = parseInt(1);
+    cartRemoveBtn.innerText = 'Remove';
 
-        cartProduct.appendChild(cartImage)
+    // Append child
+    cartItem.appendChild(cartProduct);
+    cartItem.appendChild(cartQuantity);
 
-        cartProduct.appendChild(cartDetails);
-        cartDetails.appendChild(cartHeading);
-        cartDetails.appendChild(cartPrice);
+    cartProduct.appendChild(cartArrow);
+    cartArrow.appendChild(cartUpArrow);
+    cartArrow.appendChild(cartDownArrow);
 
-        cartProduct.appendChild(cartDetails);
+    cartProduct.appendChild(cartImage);
 
-        cartQuantity.appendChild(quantity);
-        cartQuantity.appendChild(cartRemoveBtn);
+    cartProduct.appendChild(cartImage);
 
-        cartProducts.appendChild(cartItem);
-    }
-})
+    cartProduct.appendChild(cartDetails);
+    cartDetails.appendChild(cartHeading);
+    cartDetails.appendChild(cartPrice);
 
-cartProducts.addEventListener('click', function(event) {
-    const allProducts = event.target;
-    if(allProducts.matches('button')) {
-        const removeBtnParent = allProducts.parentNode;
-        removeBtnParent.parentNode.remove()
-    }
-})
+    cartProduct.appendChild(cartDetails);
+
+    cartQuantity.appendChild(quantity);
+    cartQuantity.appendChild(cartRemoveBtn);
+
+    cartProducts.appendChild(cartItem);
+  }
+});
+
+cartProducts.addEventListener('click', function (event) {
+  const allProducts = event.target;
+  if (allProducts.matches('button')) {
+    const removeBtnParent = allProducts.parentNode;
+    removeBtnParent.parentNode.remove();
+  }
+});
